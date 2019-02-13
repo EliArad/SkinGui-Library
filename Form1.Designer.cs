57e74c0acce0ca7569d60e20c2c3c2a919011fcf
easp13 <easp13@gmail.com>
2019-02-13 11:16:17 +0200

    button and background

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿using GSkinLib;
+
+namespace TestGuiSkin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new GSkinLib.SkinButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(159, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 33);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 522);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SkinButton button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using GSkinLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestGuiSkin
+{
+    public partial class Form1 : Form
+    {
+        private Point prevLeftClick;
+        private bool isFirst = true;
+        private bool toBlock = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+             
+
+            string dir = "Images\\";
+            Bitmap back = new Bitmap(dir + "pedestal_back.png");
+            GuiBackground.CreateControlRegion(this, back);
+
+            button1.SetSkin(dir, "stow");
+        }
+    
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Check if dragging of the form has occurred
+            if (e.Button == MouseButtons.Left)
+            {
+                // If this is the first mouse move event for left click dragging of the form,
+                // store the current point clicked so that we can use it to calculate the form's
+                // new location in subsequent mouse move events due to left click dragging of the form
+                if (isFirst == true)
+                {
+                    // Store previous left click position
+                    prevLeftClick = new Point(e.X, e.Y);
+
+                    // Subsequent mouse move events will not be treated as first time, until the
+                    // left mouse click is released or other mouse click occur
+                    isFirst = false;
+                }
+
+                // On subsequent mouse move events with left mouse click down. (During dragging of form)
+                else
+                {
+                    // This flag here is to allow alternate processing for dragging the form because it
+                    // causes serious flicking when u allow every such events to change the form's location.
+                    // You can try commenting this out to see what i mean
+                    if (toBlock == false)
+                        this.Location = new Point(this.Location.X + e.X - prevLeftClick.X, this.Location.Y + e.Y - prevLeftClick.Y);
+
+                    // Store new previous left click position
+                    prevLeftClick = new Point(e.X, e.Y);
+
+                    // Allow or deny next mouse move dragging event
+                    toBlock = !toBlock;
+                }
+            }
+
+            // This is a new mouse move event so reset flag
+            else
+                isFirst = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Clicked");
+        }
+    }
+}
